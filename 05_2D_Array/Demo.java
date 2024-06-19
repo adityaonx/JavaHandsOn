7384d8f56d6f31d77bc44688bde008ad9c0411cc
Aditya Sahu <adityaonx@gmail.com>
2024-06-19 12:59:06 +0000

    Added 05_2D_Array

--- 05_2D_Array/Demo.java
+++ 05_2D_Array/Demo.java
@@ -4,28 +4,40 @@ class Demo
     public static void main(String[] a){
 
         //Storing elements in array
-        int num[] = {1,5,7};
+        int[] num = {1,5,7};
 
         //Creating an empty array
-        int num1[] = new int[4];
+        int[] num1 = new int[4];
 
         //Creating 2d Array
-        int num2[][] = new int[3][4];
+        int[][] nums = new int[3][4];
 
         for (int i=0;i<3;i++){
             for(int j=0;j<4;j++){
                 //Storing random values using Math library
-                num2[i][j]= (int)(Math.random()*10);
+                nums[i][j]= (int)(Math.random()*10);//TypeCasting to int from double
             }
         }
 
         //Printing the array values
         for (int i=0;i<3;i++){
             for(int j=0;j<4;j++){
-                System.out.print(num2[i][j] + " ");
+                System.out.print(nums[i][j] + " ");
             }
             System.out.println("");
         }
+
+        System.out.println(" ");
+        
+        //Enhanced ForLoop : Printing whole array row wise
+        for ( int[] n : nums){ //here n is a 1D Array
+            for( int m : n)
+            {
+                System.out.print(m+" ");
+            }
+            System.out.println();
+        }
+
     }
 
 
